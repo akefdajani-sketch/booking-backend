015604b48a7a7877fc72e3dca8a2656b96ba7414
akefdajani-sketch <akefdajani@gmail.com>
2025-12-28 00:46:15 +0300

    Update tenants.js

--- routes/tenants.js
+++ routes/tenants.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const { pool } = require("../db");
+const db = pool;
 
 // GET /api/tenants
 router.get("/", async (req, res) => {
