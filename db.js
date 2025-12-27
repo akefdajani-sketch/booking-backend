d4fedf0b579e7e867c15fa01072031efd43f8726
akefdajani-sketch <akefdajani@gmail.com>
2025-12-28 01:30:53 +0300

    Update db.js

--- db.js
+++ db.js
@@ -2,16 +2,18 @@
 const { Pool } = require("pg");
 
 const isProd = process.env.NODE_ENV === "production";
-
-// Default to SSL in prod unless explicitly disabled
-const useSSL =
-  process.env.DATABASE_SSL
-    ? process.env.DATABASE_SSL === "true"
-    : isProd;
+const useSSL = process.env.DATABASE_SSL
+  ? process.env.DATABASE_SSL === "true"
+  : isProd;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: useSSL ? { rejectUnauthorized: false } : false,
 });
 
-module.exports = { pool };
+// ✅ Backward compatible: db.query + db.connect still work
+module.exports = {
+  pool,
+  query: (...args) => pool.query(...args),
+  connect: (...args) => pool.connect(...args),
+};
