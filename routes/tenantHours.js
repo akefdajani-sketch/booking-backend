9e137d620c241220ca925156cdd23d669db1d9b2
akefdajani-sketch <akefdajani@gmail.com>
2025-12-27 23:49:11 +0300

    Refactor database import to use pool

--- routes/tenantHours.js
+++ routes/tenantHours.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 
-const db = require("../db");
+const { pool } = require("../db");
 const requireAdmin = require("../middleware/requireAdmin");
 const { getTenantIdFromSlug } = require("../utils/tenants");
 
