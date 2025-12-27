2c929640178b77ba7b6f15d9b2df30432cd21309
akefdajani-sketch <akefdajani@gmail.com>
2025-12-27 03:10:42 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,9 @@
-// db.js
+// src/db.js
 const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: process.env.DATABASE_SSL === "true" ? { rejectUnauthorized: false } : false,
 });
 
-module.exports = pool;
+module.exports = { pool };
