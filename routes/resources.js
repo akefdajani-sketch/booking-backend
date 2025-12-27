751acedcbfed7f104e73e7936850a52374522852
akefdajani-sketch <akefdajani@gmail.com>
2025-12-27 23:48:05 +0300

    Refactor database import to use pool

--- routes/resources.js
+++ routes/resources.js
@@ -1,7 +1,7 @@
 // routes/resources.js
 const express = require("express");
 const router = express.Router();
-const db = require("../db");
+const { pool } = require("../db");
 
 const requireAdmin = require("../middleware/requireAdmin");
 
