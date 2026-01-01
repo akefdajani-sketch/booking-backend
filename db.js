43f5fcd1caa5b6c1125a32ee6f64985f420819fc
akefdajani-sketch <akefdajani@gmail.com>
2026-01-01 12:16:06 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,14 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_SSL === "true" ? { rejectUnauthorized: false } : false,
+  ssl: useSSL ? { rejectUnauthorized: false } : false,
+  max: 5,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
+});
+
+pool.on("error", (err) => {
+  console.error("PG POOL ERROR", err);
 });
 
 // Convenience helpers so routes can do: db.query(...) and db.connect()
