a8166aa28cafb65bdc163286676dedc76c9a13ef
akefdajani-sketch <akefdajani@gmail.com>
2026-01-17 02:30:29 +0300

    Update services.js

--- routes/services.js
+++ routes/services.js
@@ -142,7 +142,7 @@ router.post("/", requireAdmin, async (req, res) => {
           is_active
         )
       VALUES
-        ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+        ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
       RETURNING
         id,
         tenant_id,
