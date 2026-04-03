9c18a0d155ca8b4f652c5ca5218b665a2e69bcad
akefdajani-sketch <akefdajani@gmail.com>
2026-04-03 20:06:38 +0300

    Add files via upload

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,16 @@ module.exports = {
     "utils/**/*.js",
     "!utils/algolia/**",
     "!**/*.test.js",
+    // Sub-route directories are loaded through barrel files which are tested:
+    "!routes/bookings/**",
+    "!routes/customerMemberships/**",
+    "!routes/customers/**",
+    "!routes/services/**",
+    "!routes/tenants/**",
+    "!routes/tenantUsers/**",
+    "!routes/tenantStaffSchedule/**",
+    "!routes/tenantPrepaidAccounting/**",
+    "!routes/adminTenantsTheme/**",
   ],
   coverageThreshold: {
     global: {
